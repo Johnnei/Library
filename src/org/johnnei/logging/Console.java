4d0d6e25171242c8d7438e7d8b10ccc419fcbb0a
Johnnei <johnnei07@live.nl>
2014-10-01 09:36:08 +0200

    Added a logging package with console logging functionality

---
+++ src/org/johnnei/logging/Console.java
@@ -0,0 +1,117 @@
+package org.johnnei.logging;
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * A Formatter which is used for the {@link Logger} instances to log data to the console.
+ * @author Johnnei
+ *
+ */
+public class Console {
+	
+	/**
+	 * Creates or re-uses an existing logger<br/>
+	 * Calls <code>obtainLogger(String, Level, Formatter)</code> with <code>name</code>, <code>Level.INFO</code>, <code>new ConsoleFormatter()</code>
+	 * @param name the name of the logger
+	 * @return the logger instance
+	 * @see ConsoleFormatter
+	 */
+	public static Logger obtainLogger(String name) {
+		return obtainLogger(name, Level.INFO, new ConsoleFormatter());
+	}
+	
+	/**
+	 * Creates or re-uses an existing logger<br/>
+	 * Calls <code>obtainLogger(String, Level, Formatter)</code> with <code>name</code>, <code>minLevel</code>, <code>new ConsoleFormatter()</code>
+	 * @param name the name of the logger
+	 * @param minLevel the minimum reporting level
+	 * @return the logger instance
+	 * @see ConsoleFormatter
+	 */
+	public static Logger obtainLogger(String name, Level minLevel) {
+		return obtainLogger(name, minLevel, new ConsoleFormatter());
+	}
+	
+	/**
+	 * Creates or re-uses an existing logger
+	 * @param name The name of the logger
+	 * @param minLevel the minimum reporting level
+	 * @param format the format of the logging output
+	 * @return the logger instance
+	 */
+	public static Logger obtainLogger(String name, Level minLevel, Formatter format) {
+		Logger log = Logger.getLogger(name);
+		
+		Handler[] logHandlers = log.getHandlers();
+		log.setUseParentHandlers(false);
+		log.setLevel(minLevel);
+		if (logHandlers.length == 0) {
+			ConsoleHandler consoleHandler = new ConsoleHandler();
+			consoleHandler.setFormatter(format);
+			consoleHandler.setLevel(minLevel);
+			log.addHandler(consoleHandler);
+		} else {
+			for (Handler handler : logHandlers) {
+				if (!(handler instanceof ConsoleHandler)) {
+					continue;
+				}
+				
+				handler.setFormatter(format);
+				handler.setLevel(minLevel);
+			}
+		}
+		
+		return log;
+	}
+	
+	/**
+	 * Creates a logger based on the name.<br/>
+	 * The logger will only output if the logged message is at least the given minLevel.<br/>
+	 * As second request of the same logger will NOT update the minLevel
+	 * @param name The name of the logger
+	 * @param minLevel The minimum level to log
+	 * @return A logger (possibly re-used) with the given name
+	 */
+	public static Logger createLogger(String name, Level minLevel) {
+		Logger logger = Logger.getLogger(name);
+		
+		if (logger.getHandlers().length == 0) {
+			// This logger has no handlers/formatters, Create the handler
+			logger.setUseParentHandlers(false);
+			logger.setLevel(minLevel);
+			ConsoleHandler consoleHandler = new ConsoleHandler();
+			consoleHandler.setFormatter(new ConsoleFormatter());
+			consoleHandler.setLevel(minLevel);
+			logger.addHandler(consoleHandler);
+		}
+		return logger;
+	}
+	
+	/**
+	 * Updates the logging level to the given level
+	 * @param logName The name of the logger to change
+	 * @param minLevel The new reporting level
+	 */
+	public static void updateLogLevel(String logName, Level minLevel) {
+		updateLogLevel(Logger.getLogger(logName), minLevel);
+	}
+	
+	/**
+	 * Updates the logging level to the given level
+	 * @param log the logger to change
+	 * @param minLevel the new logging level
+	 */
+	public static void updateLogLevel(Logger log, Level minLevel) {
+		log.setLevel(minLevel);
+		for (Handler handler : log.getHandlers()) {
+			handler.setLevel(minLevel);
+		}
+	}
+
+}
+
---
+++ src/org/johnnei/logging/ConsoleFormatter.java
@@ -0,0 +1,54 @@
+package org.johnnei.logging;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+/**
+ * Used as a simple console logging format.<br/>
+ * Uses {@link SimpleDateFormatter} to print logs in: <code>[dd-MM-yyyy HH:mm:ss] [level] [name] Message</code> 
+ * @author Johnnei
+ *
+ */
+public class ConsoleFormatter extends Formatter {
+	
+	public ConsoleFormatter() {
+	}
+	
+	/**
+	 * Defines the format of the console output
+	 */
+	@Override
+	public String format(LogRecord record) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(generateDateTimeStamp());
+		sb.append(" [");
+		sb.append(record.getLevel());
+		sb.append("] [");
+		sb.append(record.getLoggerName());
+		sb.append("] ");
+		sb.append(record.getMessage());
+		sb.append(System.lineSeparator());
+		
+		if (record.getThrown() != null) {
+			sb.append("Stacktrace:" + System.lineSeparator());
+			for (StackTraceElement stackTrace : record.getThrown().getStackTrace()) {
+				sb.append(stackTrace.toString());
+				sb.append(System.lineSeparator());
+			}
+		}
+		
+		return sb.toString();
+	}
+
+	/**
+	 * Generates a datetime-stamp based on the current time
+	 * @return
+	 */
+	private String generateDateTimeStamp() {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		Date resultdate = new Date(System.currentTimeMillis());
+		return dateFormat.format(resultdate);
+	}
+}
